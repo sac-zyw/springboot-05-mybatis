12d7dabf9ddd6670115cc2df9b8f5a9c53598249
SAC <392063541@qq.com>
2020-04-26 20:32:55 +0800

    springboot整合mybatis

---
+++ src/main/java/com/sac/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.sac.controller;
+
+import com.sac.mapper.UserMapper;
+import com.sac.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserMapper userMapper;
+
+    @GetMapping("/queryUserList")
+    public List<User> queryUserList(){
+        List<User> userList = userMapper.queryUserList();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        return userList;
+    }
+}
